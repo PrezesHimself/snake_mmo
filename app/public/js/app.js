b869f99b40caf5f5e45f24c24d66b41e17eb9088
Mateusz Rorat <mateusz.rorat@contractors.roche.com>
2017-07-01 15:47:27 +0200

    progress

---
+++ app/public/js/app.js
@@ -0,0 +1,248 @@
+
+
+
+
+
+
+// debug
+
+(function () {
+    'strict mode'
+    window.DEBUG = new Debug(
+        $(".debug")
+    );
+
+    function Debug(element, config) {
+        var _self = this;
+
+        this.log = log;
+        this.listen = listen;
+        
+        
+        function listen(socket) {
+            socket.on('debug_log', function (msg) {
+                _self.log(msg);
+            });
+        }
+        
+        function log(msg) {
+            element.prepend(
+                createElement(msg)
+            )
+        }
+
+        function createElement(msg) {
+            return $('<div>').text(moment().format('MMMM Do YYYY, h:mm:ss a') + ' ' + msg);
+        }
+
+    };
+
+})();
+
+
+
+/**
+ * A lightweight game wrapper
+ *
+ * @constructor
+ */
+function Game(canvas, socket, name, options) {
+    this.canvas = canvas;
+    this.socket = socket;
+    this.name = name;
+    this.context = canvas.getContext('2d');
+
+    this.options = {
+        fps: 5
+    };
+
+    this.state = {};
+
+    socket.on('game_gameLoop', this.gameLoop.bind(this))
+}
+
+
+/**
+ * Start the game loop
+ * and initialize the keybindings
+ */
+Game.prototype.start = function () {
+    this.keyBindings();
+};
+
+
+/**
+ * Stop the game loop
+ */
+Game.prototype.stop = function() {
+    this.pause = true;
+};
+
+
+/**
+ * Scale the canvas element
+ * in accordance with the correct ratio
+ */
+Game.prototype.scale = function () {
+    this.ratio = innerWidth < innerHeight ? innerWidth : innerHeight;
+    this.tile = (this.ratio / 10) | 0;
+    this.grid = this.ratio / this.tile;
+
+    this.canvas.width = this.canvas.height = this.ratio;
+};
+
+
+/**
+ * Adds an entity to the game
+ *
+ * @param {Function} entity
+ */
+Game.prototype.addEntity = function (entity) {
+    this.entities.push(entity);
+};
+
+/**
+ * Emits to socket server
+ *
+ * @param {Function} entity
+ */
+Game.prototype.emitSocket = function (event, msg) {
+    msg.snake = {};
+    msg.snake.name = this.name;
+    this.socket.emit(event, msg);
+};
+
+
+/**
+ * Determines if an entity collides with another
+ *
+ * @param {Object} a
+ * @param {Object} b
+ */
+Game.prototype.collide = function(a, b){
+    return a.x === b.x && a.y === b.y;
+};
+
+
+/**
+ * Tracks the pressed keys
+ */
+Game.prototype.keyBindings = function () {
+    var that = this;
+
+    // define some keys
+    var keys = {
+        a: 65,
+        left: 37,
+        d: 68,
+        right: 39,
+        w: 87,
+        up: 38,
+        s: 83,
+        down: 40
+    };
+
+
+    /**
+     * Attach keyboard arrows to snake direction
+     */
+    document.onkeydown = function (e) {
+        switch ((e.which || e.keyCode) | 0) {
+            case keys.a:
+            case keys.left:
+                if (that.key !== 'right') {
+                    that.key = 'left';
+                    that.emitSocket('directionChanged', { direction: 'left'});
+                }
+                break;
+
+            case keys.d:
+            case keys.right:
+                if (that.key !== 'left') {
+                    that.key = 'right';
+                    that.emitSocket('directionChanged', { direction: 'right'});
+                }
+                break;
+
+            case keys.w:
+            case keys.up:
+                if (that.key !== 'down') {
+                    that.key = 'up';
+                    that.emitSocket('directionChanged', { direction: 'up'});
+                }
+                break;
+
+            case keys.s:
+            case keys.down:
+                if (that.key !== 'up') {
+                    that.key = 'down';
+                    that.emitSocket('directionChanged', { direction: 'down'});
+                }
+        }
+    };
+
+};
+
+
+/**
+ * The gameloop - and entity (update/draw) calls
+ * Use of `setTimeout` instead of animationFrame
+ * in order to keep it simple as possible
+ */
+Game.prototype.gameLoop = function (state) {
+    this.state = state;
+    this.draw();
+};
+
+/**
+ * draw
+ */
+Game.prototype.draw = function () {
+    var _self = this;
+
+
+    this.context.fillStyle = '#000';
+    this.context.globalAlpha   = .2;
+    this.context.fillRect(
+        0, 0, this.canvas.width, this.canvas.height);
+    this.context.globalAlpha   = 1;
+
+    _.each(this.state.snakes, function (snake) {
+        var grid = _self.grid;
+        _self.context.fillStyle = '#0ae';
+        _self.context.fillRect(
+            snake.x * grid,
+            snake.y * grid,
+            grid, grid);
+    });
+};
+
+
+$('.snake-name-button').click(function () {
+    $('.splash').toggle();
+    var name = $('.snake-name').val();
+
+    var socket = io();
+
+    DEBUG.listen(socket);
+
+    socket.emit('joinGame', {name: name});
+    
+    
+    // create the canvas element
+    var canvas = document.createElement("canvas");
+    document.body.appendChild(canvas);
+    //
+    // /**
+    //  * Game initialization
+    //  * and entity preparation
+    //  */
+    var game = new Game(canvas, socket, name);
+    game.scale();
+    // var food = new Food(game);
+    // var snake = new Snake(game, food);
+    //
+    // game.addEntity(food);
+    // game.addEntity(snake);
+    game.start();
+});
--- app/server/index.js
+++ app/server/index.js
@@ -1,18 +1,25 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const _ = require('lodash');
+var game;
+var debug;
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/html/index.html');
-});
+app.use('/', express.static('./app/public'))
 
 io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
+
+    socket.on('joinGame', function(snake){
+        game.addSnake(snake);
     });
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
+
+    socket.on('directionChanged', function(direction){
+        debug.log(`${direction.snake.name} has moved ${direction.direction}`);
+
+        var snake = _.find(game.snakes, {name: direction.snake.name});
+
+        if(snake) snake.direction = direction.direction;
     });
 });
 
@@ -21,3 +28,131 @@ io.on('connection', function(socket){
 http.listen(process.env.PORT || 5000, function(){
     console.log(`listening on *:${process.env.PORT || 5000}`);
 });
+
+
+(function () {
+    'strict mode'
+    global.DEBUG = {};
+
+    global.DEBUG.Debug = function Debug(io, config) {
+
+        this.log = log;
+
+        function log(msg) {
+            io.sockets.emit('debug_log', msg);
+        }
+
+    };
+
+})();
+
+(function () {
+    'strict mode'
+    global.GAME = {};
+
+    global.GAME.Game = Game;
+
+    function Game(io, debug) {
+        this.io = io;
+        this.debug = debug;
+
+        this.snakes = [];
+
+        this.options = {
+            fps: 15
+        };
+        this.state = {};
+    };
+    
+    Game.prototype.start = function (event) {
+        this.gameLoop();
+    };
+
+    Game.prototype.getState = function () {
+        var state = _.reduce(this.snakes, function (sum, n) {
+            return `${sum} ${n.name} is on x: ${n.x} y: ${n.y}`;
+        }, '');
+
+        return state;
+    };
+
+    Game.prototype.getStateObj = function () { //change that to class
+        var state = {};
+        state.snakes = this.snakes;
+        return state;
+    };
+
+    Game.prototype.addSnake = function (snake) {
+        this.debug.log(snake.name + ' joined the game');
+        this.snakes.push(new Snake(snake.name));
+    };
+
+    Game.prototype.broadcastSocket = function (event, payload) {
+        this.io.sockets.emit(event, payload);
+    };
+
+    Game.prototype.update = function () {
+        _.each(this.snakes, function (snake) {
+            snake.update();
+        })
+    }
+
+    Game.prototype.gameLoop = function () {
+        const _self = this;
+
+        this.update();
+
+        this.broadcastSocket('game_gameLoop', this.getStateObj());
+        this.debug.log(this.getState());
+
+        setTimeout(function(){
+            _self.gameLoop();
+        }, 1000 / this.options.fps);
+    };
+    
+//    snake
+
+    function Snake(name) {
+        this.x = _.sample([10, 20, 30, 40]);
+        this.y = _.sample([10, 20, 30, 40]);
+        this.name = name;
+        this.direction = 'right';
+    }
+
+    Snake.prototype.update = function () {
+        switch (this.direction) {
+            case 'up':
+                this.y -= 1;
+                break;
+            case 'down':
+                this.y += 1;
+                break;
+            case 'left':
+                this.x -= 1;
+                break;
+            case 'right':
+                this.x += 1;
+                break;
+        }
+
+        if(this.y < 0) this.y = global.GRID.height;
+        if(this.y > global.GRID.height) this.y = 0;
+        if(this.x < 0) this.x = global.GRID.width;
+        if(this.x > global.GRID.width) this.x = 0;
+
+    }
+
+//    grid 
+    global.GRID = new Grid();
+
+    function Grid() {
+        this.width = 67;
+        this.height = 67;
+    }
+
+})();
+
+
+debug = new DEBUG.Debug(io);
+game = new global.GAME.Game(io, debug);
+game.start();
